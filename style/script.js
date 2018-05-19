ab35d73b51ce50ed9eaf1da99da29ff48c9c3c78
Armin <dead4game@gmail.com>
2018-05-19 16:19:10 +0430

    Initial commit

---
+++ style/script.js
@@ -0,0 +1,16 @@
+﻿function show(i) {
+    bigi.src = "pic/car" + i + ".jpg ";
+}
+
+m1.onclick = function() {
+    longman.style.left = "0";
+}
+m2.onclick = function() {
+    longman.style.left = "-100%";
+}
+m3.onclick = function() {
+    longman.style.left = "-200%";
+}
+m4.onclick = function() {
+    longman.style.left = "-300%";
+}
